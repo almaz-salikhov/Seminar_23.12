293c19fa22b9fa00631866ccc3b8e52b9de08945
Almaz Salikhov <almaz.salikhov@gmail.com>
2022-12-23 20:49:23 +0200

    Split Method Example

---
+++ SplitMethod/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите элементы через пробел: ");
+int[] arr2 = Console.ReadLine()
+            .Split(',')
+            .Select(Int32.Parse)
+            .Where(e => e % 2 == 0)
+            .ToArray();
+
+string[] arr3 = Console.ReadLine().Split(',');
